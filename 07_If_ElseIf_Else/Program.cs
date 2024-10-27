d511358de52804af5b1e40de3120473a36edddbc
Altan Emre <altan_emre_1989@hotmail.com>
2024-10-27 18:41:50 +0300

    if else Completed

--- 07_If_ElseIf_Else/Program.cs
+++ 07_If_ElseIf_Else/Program.cs
@@ -209,32 +209,218 @@
             #endregion
 
 
-            Console.WriteLine("Birinci ürün fiyatını giriniz");
-            double fiyat1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Birinci ürün fiyatını giriniz");
+            //double fiyat1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("İkinci ürün fiyatını giriniz");
-            double fiyat2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("İkinci ürün fiyatını giriniz");
+            //double fiyat2 = Convert.ToDouble(Console.ReadLine());
 
 
-            if ((fiyat1 + fiyat2) > 2500)
+            //if ((fiyat1 + fiyat2) > 2500)
+            //{
+            //    if (fiyat1 > fiyat2)
+            //    {
+            //        fiyat2 = fiyat2 * 0.75;
+            //    }
+            //    else
+            //    {
+            //        fiyat1 = fiyat1 * 0.75;
+            //    }
+            //}
+            //Console.WriteLine("Ödeme:" + (fiyat1 + fiyat2));
+
+            #endregion
+
+            #region // Aylık geliri 40000 üstünde ise %12 vergi kesilecek,
+            // 40000 ve altında ise %9 vergi kesimi yapılarak 
+            // kullanıcıya yeni gelirini bu hesaplamalar sonucunda gösteriniz
+
+            //Console.WriteLine("Aylik gelirinizi giriniz");
+            //double aylikgelir = Convert.ToDouble(Console.ReadLine());
+
+            //if (aylikgelir > 40000)
+            //{
+            //    aylikgelir = aylikgelir * 0.88;                
+            //}
+            //else 
+            //{
+            //    double vergi = aylikgelir * 0.09;
+            //    aylikgelir = aylikgelir - vergi;
+
+            //    //aylikgelir = aylikgelir * 0.91;               
+            //}
+            //Console.WriteLine("Yeni geliriniz:" + aylikgelir);
+
+            #endregion
+
+            #region Öğrenciden vize ve final notlarını alınız ve vize %40 final %60 alınarak ortalamasını hesaplayınız. 
+            //ortalamaya göre 
+            /*
+            0,24   FF
+            25,44  DD
+            45,54  CC
+            55,69  CB
+            70,84  BB
+            85,100 AA
+
+             */
+            //BASLA:
+            //    Console.WriteLine("Öğrencinin vize notunu giriniz.");
+            //    int vizenotu = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Öğrencinin final notunu giriniz.");
+            //    int finalnotu = Convert.ToInt32(Console.ReadLine());
+
+
+            //    if(vizenotu<0 || vizenotu>100 || finalnotu>100 || finalnotu < 0)
+            //    {
+            //        Console.WriteLine("Hatalı Not Girişi!!");
+            //        goto BASLA;
+            //    }
+
+            //double ortalamanot = vizenotu * 0.4 + finalnotu * 0.6;
+
+
+            // if (ortalamanot >= 0 && ortalamanot <= 24)
+            //{
+
+            //    Console.WriteLine("FF");
+            //}
+
+            //else if (ortalamanot >= 25 && ortalamanot <= 44)
+            //{
+
+            //    Console.WriteLine("DD");
+            //}
+
+            //else if (ortalamanot >= 45 && ortalamanot <= 54)
+            //{
+
+            //    Console.WriteLine("CC");
+            //}
+
+            //else if (ortalamanot >= 55 && ortalamanot <= 69)
+            //{
+
+            //    Console.WriteLine("CB");
+            //}
+
+            //else if (ortalamanot >= 70 && ortalamanot <= 84)
+            //{
+
+            //    Console.WriteLine("BB");
+            //}
+            //else
+            //{
+
+            //    Console.WriteLine("AA");
+            //}
+
+
+            //goto BASLA;
+
+
+            #region Deniz
+            //Console.WriteLine("Vize Puanınızı Giriniz");
+            //double vize = Convert.ToDouble(Console.ReadLine());
+
+            //if (vize >= 0 && vize <= 100)
+            //{
+            //    Console.WriteLine("Final Notunuzu Giriniz");
+            //    double final = Convert.ToDouble(Console.ReadLine());
+
+            //    if (final >= 0 && final <= 100)
+            //    {
+            //        double ortalama = vize * 0.4 + final * 0.6;
+            //        if (ortalama < 25)
+            //            Console.WriteLine("FF");
+            //        else if (ortalama < 45)
+            //            Console.WriteLine("DD");
+            //        else if (ortalama < 55)
+            //            Console.WriteLine("CC");
+            //        else if (ortalama < 70)
+            //            Console.WriteLine("CB");
+            //        else if (ortalama < 85)
+            //            Console.WriteLine("BB");
+            //        else
+            //            Console.WriteLine("AA");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Lütfen Geçerli Final Notu Giriniz");
+            //    }
+
+            //}
+
+            //else Console.WriteLine("Lütfen Geçerli Vize Not Giriniz");
+            #endregion
+            #endregion
+
+            #region  1-50 arasındaki sayıların içinde 7'e tam bölünenleri ekrana teker teker yazdırınız.
+
+            //int i = 1;
+
+            //DON:
+            //if (i < 51)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //    goto DON;
+            //}
+
+            #endregion
+
+            #region   Kullanıcıdan isim, yaş, maaş ve çocuk sayısı alınsın.
+            /*
+                Eğer kulanıcının yaşı 45'in altındaysa;
+                    Çocuk sayısına bakılacak.Ve çocuk sayısı 3'ten az ise çocuk başına 2500₺,
+                                                          3 ve 3'ten çok ise çocuk başına 2000₺ 
+                                                                maaşa ekleme yapılacak.
+                45 ve 45'in üzerinde ise çocuk başına para verilmeyecek ancak 5000₺ ekleme yapılacak.
+                Son olarak ekrana: "Nesrin Yılmaz, Maaşınız: 40000₺" yazılacak.
+             */
+
+
+            Console.WriteLine("İsminiz:");
+            string isim = Console.ReadLine();
+
+            Console.WriteLine("Yaş:");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Maaş:");
+            double maas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Çocuk Sayısı:");
+            int cocukSayisi = Convert.ToInt32(Console.ReadLine());
+
+            if (yas < 45 && yas>17) 
             {
-                if (fiyat1 > fiyat2)
+                if (cocukSayisi < 3)
                 {
-                    fiyat2 = fiyat2 * 0.75;
+                    maas += cocukSayisi * 2500; //maas = maas + (cocukSayisi*2500)
                 }
                 else
                 {
-                    fiyat1 = fiyat1 * 0.75;
+                    maas += cocukSayisi * 2000;
                 }
+                Console.WriteLine($"Sayın {isim} Maaşınız: {maas}");
             }
-            Console.WriteLine("Ödeme:" + (fiyat1 + fiyat2));
-
-
-
-
-
-
+            else if(yas<70 && yas>44) 
+            {
+                maas += 5000;
+                Console.WriteLine($"Sayın {isim} Maaşınız: {maas}");
+            }
+            else
+            {
+                Console.WriteLine("Senin yazacağın yaş bu olmamalı!!");
+            }
+            
             #endregion
+
+
         }
     }
 }
